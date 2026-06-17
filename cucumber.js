749f5db77097659cf0c26162cfa2ccb98b3eff4a
vanessa-gomes1 <meunomesvanessagomes>
2026-06-17 14:21:42 -0300

    atualização gitignore

--- cucumber.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  default: {
-    timeout: 30000, // Aumenta o timeout para 30 segundos
-    paths: ['features/**/*.feature'],
-    require: ['steps/**/*.steps.js', 'support/**/*.js'],
-    format: ['json:reports/cucumber-report.json']
-  }
-};
